abd90e1113bb4e9e68dc458e2d2ed16d4c0648e0
Eduardohcg10 <eduardohcg10@gmail.com>
2022-06-13 20:46:08 -0300

    Desafio for/while

---
+++ DesafioForWhile/script.js
@@ -0,0 +1,30 @@
+console.log("For de 1 a 10")
+for(let count = 1; count <= 10; count ++){
+    console.log(count)
+}
+console.log("While de 10 a 1")
+let count = 10
+while (count >=1){
+    console.log(count)
+    count -=1
+}
+console.log("While Numeros Impares 0 a 100")
+let count2 = 1
+while (count2 <=100){
+    console.log(count2)
+    count2 += 2
+}
+console.log("For Numeros Impares 0 a 100")
+for (let count2 = 1; count2 <=100; count2 +=2){
+    console.log(count2)
+}
+console.log("While Numeros pares 0 a 100")
+let count3 = 0
+while (count3 <=100){
+    console.log(count3)
+    count3 += 2
+}
+console.log("For Numeros pares 0 a 100")
+for (let count3 = 0; count3 <= 100; count3 +=2){
+    console.log(count3)
+}
